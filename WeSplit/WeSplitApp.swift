88e0c8821ce82fc945140cfdc3535f7b67eaa761
Caio <caiofmart@gmail.com>
2022-01-31 23:01:52 -0300

    Open commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Caio Feitoza Martins on 11/06/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
